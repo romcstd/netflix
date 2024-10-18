356a81eba9fca9c68a5eff751d35cea8c99b34f6
Romel Custodio <romelroxascustodio@gmail.com>
2024-10-18 11:21:51 +0800

    Release-v1.2 | Header

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,12 @@
+import Head from "next/head";
+
 export default function Login() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Head>
+        <title>My page title</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         LOGIN
       </main>
